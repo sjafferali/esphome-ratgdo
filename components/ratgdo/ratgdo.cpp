6850d24ec8f4e8059e747c34b7a6154262e8e221
Blazer Silving <breakingspell@gmail.com>
2025-04-01 17:55:53 -0500

    Set door state to OPEN after position STOP
    Use position float instead of 1.0 for open state

--- components/ratgdo/ratgdo.cpp
+++ components/ratgdo/ratgdo.cpp
@@ -268,7 +268,7 @@ namespace ratgdo {
             this->cancel_position_sync_callbacks();
             cancel_timeout("door_query_state");
         } else if (door_state == DoorState::OPEN) {
-            this->door_position = 1.0;
+            this->door_position_update();
             this->cancel_position_sync_callbacks();
         } else if (door_state == DoorState::CLOSED) {
             this->door_position = 0.0;
@@ -736,6 +736,8 @@ namespace ratgdo {
         this->door_action(delta > 0 ? DoorAction::OPEN : DoorAction::CLOSE);
         set_timeout("move_to_position", operation_time, [this] {
             this->door_action(DoorAction::STOP);
+            this->received(DoorState::OPEN);
+            this->cancel_position_sync_callbacks();
         });
     }
 
