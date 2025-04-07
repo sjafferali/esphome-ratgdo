7da19257ea47218278765e919e98f42d04d70391
Blazer Silving <breakingspell@gmail.com>
2025-04-06 19:18:19 -0500

    Tentatively move OPEN state override to door_action check

--- components/ratgdo/ratgdo.cpp
+++ components/ratgdo/ratgdo.cpp
@@ -703,6 +703,14 @@ namespace ratgdo {
 #else
         this->protocol_->door_action(action);
 #endif
+#ifdef PROTOCOL_DRYCONTACT
+        if (action == DoorAction::STOP || action == DoorAction::TOGGLE) {
+            // Door will likely be open partially unless closed
+            if (*this->door_state == DoorState::OPENING || *this->door_state == DoorState::CLOSING) {
+                this->received(DoorState::OPEN);
+            }
+        }
+#endif
     }
 
     void RATGDOComponent::door_move_to_position(float position)
@@ -736,8 +744,6 @@ namespace ratgdo {
         this->door_action(delta > 0 ? DoorAction::OPEN : DoorAction::CLOSE);
         set_timeout("move_to_position", operation_time, [this] {
             this->door_action(DoorAction::STOP);
-            this->received(DoorState::OPEN);
-            this->cancel_position_sync_callbacks();
         });
     }
 
